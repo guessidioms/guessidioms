28188125144eeaa27b8df8b6f8450a304ee17766
jiahao.xu <jiahao.xu@shopee.com>
2022-04-05 00:33:22 +0800

    can play

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
+import base64
 from flask import Flask, render_template, flash, redirect, url_for, Markup
+import base64
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'secret string')
@@ -9,8 +11,11 @@ app.jinja_env.lstrip_blocks = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', code="花好月圆")
 
+@app.route('/<code>')
+def guess(code):
+    return render_template('index.html', code=base64.b16decode(code).decode("utf-8"))
 
 # 404 error handler
 @app.errorhandler(404)
