237e9fc1dc6701a94297b55867cc8195bd76b2f4
jiahao.xu <jiahao.xu@shopee.com>
2022-04-09 18:05:24 +0800

    pure static version

--- app.py
+++ app.py
@@ -4,29 +4,29 @@ from __future__ import print_function
 from __future__ import division
 
 import os
-import random
-from flask import Flask, render_template, flash, redirect, url_for, Markup
-import base64
+from flask import Flask, render_template
+# import random
+# import base64
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'secret string')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
-with open("txt/idioms_reviewed.txt", "rb") as fp:
-    idioms = fp.readlines()
-idioms = [i.strip() for i in idioms]
-idioms_length = len(idioms)
+# with open("txt/idioms_reviewed.txt", "rb") as fp:
+#     idioms = fp.readlines()
+# idioms = [i.strip() for i in idioms]
+# idioms_length = len(idioms)
 
 @app.route('/')
 def index():
-    random_index = random.randint(0, idioms_length - 1)
-    idiom = idioms[random_index].decode("utf-8")
-    return render_template('index.html', code=idiom)
+    # random_index = random.randint(0, idioms_length - 1)
+    # idiom = idioms[random_index].decode("utf-8")
+    return render_template('index.html')
 
 @app.route('/<code>')
 def guess(code):
-    return render_template('index.html', code=base64.b16decode(code).decode("utf-8"))
+    return render_template('index.html')
 
 # 404 error handler
 @app.errorhandler(404)
